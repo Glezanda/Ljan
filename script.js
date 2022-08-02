75ca4d0377f3268d3e2f03a7e9404831bede3a20
Julia Melik <julija.melik@gmail.com>
2022-08-02 14:47:48 +0200

    menu

---
+++ script.js
@@ -0,0 +1,17 @@
+.menu li{
+    display: inline-block;
+} 
+
+.menu{
+    margin: 0;
+    padding:0;
+    text-alingn: center;
+}
+
+.menu a{
+    display: inline-block;
+    color: white
+    text-decoration: none;
+    padding: 20px 30px; 
+
+}
